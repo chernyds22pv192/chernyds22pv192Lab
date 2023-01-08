2291f79e03769e96b1375dc810fcfe64bfff1715
chernyds22pv192 <113597343+chernyds22pv192@users.noreply.github.com>
2023-01-08 16:57:59 +0300

    Update LendingTermsException.java

--- src/main/java/tech/reliab/course/chernyds/bank/exceptions/LendingTermsException.java
+++ src/main/java/tech/reliab/course/chernyds/bank/exceptions/LendingTermsException.java
@@ -3,5 +3,5 @@ package tech.reliab.course.chernyds.bank.exceptions;
 /**
  * Исключение для случая, когда были нарушены условия кредитования
  */
-public class LendingTermsException extends Error{
+public class LendingTermsException extends RuntimeException {
 }
